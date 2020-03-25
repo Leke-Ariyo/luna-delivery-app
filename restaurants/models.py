2d4fdecba91087d6f25fa60493f29bc203f65f4c
paulonteri <onteripaul@gmail.com>
2020-03-25 10:42:54 +0300

    Added class method __str__() for each model

--- restaurants/models.py
+++ restaurants/models.py
@@ -10,17 +10,26 @@ class Restaurant(models.Model):
     menu_list = models.ForeignKey('MenuList', on_delete=models.CASCADE,
                                   null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class MenuList(models.Model):
     name = models.CharField(max_length=255)
     menu = models.ManyToManyField(
         'Menu')
 
+    def __str__(self):
+        return f'{self.name} {self.menu}'
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     units = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Order(models.Model):
@@ -45,8 +54,17 @@ class Order(models.Model):
     delivery_company = models.ForeignKey(
         'DeliveryCompany', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.restaurant} {self.menu} {self.status}'
+
 
 class DeliveryCompany(models.Model):
     name = models.CharField(max_length=255)
     staff_id = models.PositiveIntegerField(blank=True, null=True)
     staff_name = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "DeliveryCompanies"
+
+    def __str__(self):
+        return f'{self.name} {self.staff_name}'
